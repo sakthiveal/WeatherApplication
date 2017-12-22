7899f30b904abd5a38e76772d043fa350f5721c6
sakthi veal <32634534+sakthiveal@users.noreply.github.com>
2017-12-22 17:24:19 +0530

    second

--- www/js/app.js
+++ www/js/app.js
@@ -7,24 +7,24 @@ $(document).ready(function(){
 function onDeviceReady(){
 	console.log('Device Ready...');
 
-	$('#show_more_location').click(function(e){
-		e.preventDefault();
-		getMoreLocation();
-	});
+	// $('#show_more_location').click(function(e){
+	// 	e.preventDefault();
+	// 	getMoreLocation();
+	// });
 
-	$('#clear_info').click(function(e){
-		e.preventDefault();
-		clearInfo();
-	});
+	// $('#clear_info').click(function(e){
+	// 	e.preventDefault();
+	// 	clearInfo();
+	// });
 
 	getDate();
 
 	getLocation();
 
-	$('#other_location').click(function(e){
-		e.preventDefault();
-		getOtherLocation();
-	});
+	// $('#other_location').click(function(e){
+	// 	e.preventDefault();
+	// 	getOtherLocation();
+	// });
 }
 
 // Get, Format & Display Current Date
@@ -41,155 +41,155 @@ function getDate(){
 }
 
 // Get Current User Location
-function getLocation(){
-	console.log('Getting Users Location...');
+// function getLocation(){
+// 	console.log('Getting Users Location...');
 
-	navigator.geolocation.getCurrentPosition(function(position){
-		console.log(1111111111111111);
-		var lat = position.coords.latitude;
-		var lon = position.coords.longitude;
-		var city = '';
-		var state = '';
-		var html = '';
+// 	navigator.geolocation.getCurrentPosition(function(position){
+// 		console.log(1111111111111111);
+// 		var lat = position.coords.latitude;
+// 		var lon = position.coords.longitude;
+// 		var city = '';
+// 		var state = '';
+// 		var html = '';
 
-		$.ajax({
-			url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lon,
-			datatype: 'jsonp',
-			success: function(response){
-				city = response.results[0].address_components[2].long_name;
-				state = response.results[0].address_components[4].short_name;
+// 		$.ajax({
+// 			url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lon,
+// 			datatype: 'jsonp',
+// 			success: function(response){
+// 				city = response.results[0].address_components[2].long_name;
+// 				state = response.results[0].address_components[4].short_name;
 
-				html = '<h1>'+city+', '+state+'</h1>';
+// 				html = '<h1>'+city+', '+state+'</h1>';
 
-				$('#myLocation').html(html);
+// 				$('#myLocation').html(html);
 
-				// Get Weather Info
-				getWeather(city, state);
+// 				// Get Weather Info
+// 				getWeather(city, state);
 
-				$('#show_more_weather').click(function(e){
-					e.preventDefault();
+// 				$('#show_more_weather').click(function(e){
+// 					e.preventDefault();
 
-				// Close Dropdown Menu
-				$('.navbar-toggle').click();
+// 				// Close Dropdown Menu
+// 				$('.navbar-toggle').click();
 
-					getMoreWeather(city, state);
-				});
-			}
-		});
-	}, function(err){
-		console.warn('ERROR(' + err.code + '): ' + err.message);
-	});
-}	
-
-
-// Get Some Extra Location Info
-function getMoreLocation(){
-	console.log('Getting More Location Info...');
-
-	// Close Dropdown Menu
-	$('.navbar-toggle').click();
-
-	var html = '';
-
-	navigator.geolocation.getCurrentPosition(function(position){
-		html='<ul id="more_location_list" class="list-group">' +
-		'<li class="list-group-item"><strong>Latitude: </strong>'+position.coords.latitude+'</li>' +
-		'<li class="list-group-item"><strong>Longitude: </strong>'+position.coords.longitude+'</li>' +
-		'<li class="list-group-item"><strong>Altitude: </strong>'+position.coords.altitude+'</li>' +
-		'<li class="list-group-item"><strong>Accuracy: </strong>'+position.coords.accuracy+'</li>' +
-		'</ul>';
-
-	$('#more_location_display').html(html);
-	});
-}
-
-// Get The Weather Info For a Location
-function getWeather(city, state){
-	console.log('Getting Weather For '+city+'...');
-
-	var html = '';
-	$.ajax({
-		url:'http://api.wunderground.com/api/62d0e72f66a7de1a/conditions/q/'+state+'/'+city+'.json',
-		datatype:'jsonp',
-		success: function(parsed_json){
-			console.log(parsed_json.current_observation);
-
-			weather = parsed_json['current_observation']['weather'];
-			temperature_string = parsed_json['current_observation']['temperature_string'];
-			icon_url = parsed_json['current_observation']['icon_url'];
-
-			html = '<h1 class="text-center"><img src="'+icon_url+'"> '+weather+'</h1>' +
-			'<h2 class="text-center">'+temperature_string+'</h2>';
-
-			$('#weather').html(html);
-		}
-	});
-}
+// 					getMoreWeather(city, state);
+// 				});
+// 			}
+// 		});
+// 	}, function(err){
+// 		console.warn('ERROR(' + err.code + '): ' + err.message);
+// 	});
+// }	
 
 
-// Display More Weather Info
-function getMoreWeather(city, state){
-	console.log('Getting More Weather');
+// // Get Some Extra Location Info
+// function getMoreLocation(){
+// 	console.log('Getting More Location Info...');
 
-	var html = '';
-	$.ajax({
-		url:'http://api.wunderground.com/api/62d0e72f66a7de1a/conditions/q/'+state+'/'+city+'.json',
-		datatype:'jsonp',
-		success: function(parsed_json){
-			console.log(parsed_json.current_observation);
+// 	// Close Dropdown Menu
+// 	$('.navbar-toggle').click();
 
-			temp_f = parsed_json['current_observation']['temp_f'];
-			temp_c = parsed_json['current_observation']['temp_c'];
-			dewpoint_string = parsed_json['current_observation']['dewpoint_string'];
-			dewpoint_f = parsed_json['current_observation']['dewpoint_f'];
-			dewpoint_c = parsed_json['current_observation']['dewpoint_c'];
-			wind_string = parsed_json['current_observation']['wind_string'];
-			wind_dir = parsed_json['current_observation']['wind_dir'];
-			wind_mph = parsed_json['current_observation']['wind_mph'];
-			visibility = parsed_json['current_observation']['visibility'];
-			solarradiation = parsed_json['current_observation']['solarradiation'];
-			relative_humidity = parsed_json['current_observation']['relative_humidity'];
-			local_time = parsed_json['current_observation']['local_time_rfc822'];
-			precip_today_in = parsed_json['current_observation']['precip_today_in'];
-			feelslike_string = parsed_json['current_observation']['feelslike_string'];
-			feelslike_f = parsed_json['current_observation']['feelslike_f'];
-			feelslike_c = parsed_json['current_observation']['feelslike_c'];
+// 	var html = '';
 
-			html='<ul id="more_weather_list" class="list-group">' +
-				'<li class="list-group-item"><strong>Feels Like: </strong>'+feelslike_string+'</li>' +
-				'<li class="list-group-item"><strong>Dewpoint: </strong>'+dewpoint_string+'</li>' +
-				'<li class="list-group-item"><strong>Wind: </strong>'+wind_string+'</li>' +
-				'<li class="list-group-item"><strong>Wind Speed: </strong>'+wind_mph+'</li>' +
-				'<li class="list-group-item"><strong>Humidity: </strong>'+relative_humidity+'</li>' +
-				'<li class="list-group-item"><strong>Solar Radiation: </strong>'+solarradiation+'</li>' +
-				'<li class="list-group-item"><strong>Precipitation: </strong>'+precip_today_in+'</li>' +
-				'</ul>';
+// 	navigator.geolocation.getCurrentPosition(function(position){
+// 		html='<ul id="more_location_list" class="list-group">' +
+// 		'<li class="list-group-item"><strong>Latitude: </strong>'+position.coords.latitude+'</li>' +
+// 		'<li class="list-group-item"><strong>Longitude: </strong>'+position.coords.longitude+'</li>' +
+// 		'<li class="list-group-item"><strong>Altitude: </strong>'+position.coords.altitude+'</li>' +
+// 		'<li class="list-group-item"><strong>Accuracy: </strong>'+position.coords.accuracy+'</li>' +
+// 		'</ul>';
 
-			$('#more_weather_display').html(html);
-		}
-	});
-}
+// 	$('#more_location_display').html(html);
+// 	});
+// }
 
-// Clear All Extra Info
-function clearInfo(){
-	getLocation();
+// // Get The Weather Info For a Location
+// function getWeather(city, state){
+// 	console.log('Getting Weather For '+city+'...');
 
-	$('.navbar-toggle').click();
+// 	var html = '';
+// 	$.ajax({
+// 		url:'http://api.wunderground.com/api/62d0e72f66a7de1a/conditions/q/'+state+'/'+city+'.json',
+// 		datatype:'jsonp',
+// 		success: function(parsed_json){
+// 			console.log(parsed_json.current_observation);
 
-	$('#more_weather_display').html('');
-	$('#more_location_display').html('');
-}
+// 			weather = parsed_json['current_observation']['weather'];
+// 			temperature_string = parsed_json['current_observation']['temperature_string'];
+// 			icon_url = parsed_json['current_observation']['icon_url'];
+
+// 			html = '<h1 class="text-center"><img src="'+icon_url+'"> '+weather+'</h1>' +
+// 			'<h2 class="text-center">'+temperature_string+'</h2>';
+
+// 			$('#weather').html(html);
+// 		}
+// 	});
+// }
 
 
-// Get Info About Other Location
-function getOtherLocation(){
-	var html = '';
-	var city = $('#city').val();
-	var state = $('#state').val();
+// // Display More Weather Info
+// function getMoreWeather(city, state){
+// 	console.log('Getting More Weather');
 
-	html = '<h1>'+city+', '+state+'</h1>';
+// 	var html = '';
+// 	$.ajax({
+// 		url:'http://api.wunderground.com/api/62d0e72f66a7de1a/conditions/q/'+state+'/'+city+'.json',
+// 		datatype:'jsonp',
+// 		success: function(parsed_json){
+// 			console.log(parsed_json.current_observation);
 
-	$('#myLocation').html(html);
+// 			temp_f = parsed_json['current_observation']['temp_f'];
+// 			temp_c = parsed_json['current_observation']['temp_c'];
+// 			dewpoint_string = parsed_json['current_observation']['dewpoint_string'];
+// 			dewpoint_f = parsed_json['current_observation']['dewpoint_f'];
+// 			dewpoint_c = parsed_json['current_observation']['dewpoint_c'];
+// 			wind_string = parsed_json['current_observation']['wind_string'];
+// 			wind_dir = parsed_json['current_observation']['wind_dir'];
+// 			wind_mph = parsed_json['current_observation']['wind_mph'];
+// 			visibility = parsed_json['current_observation']['visibility'];
+// 			solarradiation = parsed_json['current_observation']['solarradiation'];
+// 			relative_humidity = parsed_json['current_observation']['relative_humidity'];
+// 			local_time = parsed_json['current_observation']['local_time_rfc822'];
+// 			precip_today_in = parsed_json['current_observation']['precip_today_in'];
+// 			feelslike_string = parsed_json['current_observation']['feelslike_string'];
+// 			feelslike_f = parsed_json['current_observation']['feelslike_f'];
+// 			feelslike_c = parsed_json['current_observation']['feelslike_c'];
 
-	getWeather(city, state);
-}
+// 			html='<ul id="more_weather_list" class="list-group">' +
+// 				'<li class="list-group-item"><strong>Feels Like: </strong>'+feelslike_string+'</li>' +
+// 				'<li class="list-group-item"><strong>Dewpoint: </strong>'+dewpoint_string+'</li>' +
+// 				'<li class="list-group-item"><strong>Wind: </strong>'+wind_string+'</li>' +
+// 				'<li class="list-group-item"><strong>Wind Speed: </strong>'+wind_mph+'</li>' +
+// 				'<li class="list-group-item"><strong>Humidity: </strong>'+relative_humidity+'</li>' +
+// 				'<li class="list-group-item"><strong>Solar Radiation: </strong>'+solarradiation+'</li>' +
+// 				'<li class="list-group-item"><strong>Precipitation: </strong>'+precip_today_in+'</li>' +
+// 				'</ul>';
+
+// 			$('#more_weather_display').html(html);
+// 		}
+// 	});
+// }
+
+// // Clear All Extra Info
+// function clearInfo(){
+// 	getLocation();
+
+// 	$('.navbar-toggle').click();
+
+// 	$('#more_weather_display').html('');
+// 	$('#more_location_display').html('');
+// }
+
+
+// // Get Info About Other Location
+// function getOtherLocation(){
+// 	var html = '';
+// 	var city = $('#city').val();
+// 	var state = $('#state').val();
+
+// 	html = '<h1>'+city+', '+state+'</h1>';
+
+// 	$('#myLocation').html(html);
+
+// 	getWeather(city, state);
+// }
